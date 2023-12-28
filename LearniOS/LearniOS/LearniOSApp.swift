fe21b8801061a1e22aa3045b7a9032ba38c1e4f1
Naresh Kumar <nareshkumar77888@gmail.com>
2023-12-28 14:13:47 +0530

    Learn-iOS # Project created

---
+++ LearniOS/LearniOS/LearniOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearniOSApp.swift
+//  LearniOS
+//
+//  Created by Naresh on 28/12/23.
+//
+
+import SwiftUI
+
+@main
+struct LearniOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
